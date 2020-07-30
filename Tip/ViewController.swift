32857c66f10e7d34fc04cc2f82c8b1cacc6f09a3
maoki109 <mariaoki109@gmail.com>
2020-07-30 08:51:44 -0600

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Mari Aoki on 7/30/20.
+//  Copyright © 2020 Mari Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
